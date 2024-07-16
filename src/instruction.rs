768ab11e775065041d00a4e19340bdb9c7d78c72
wpkelso <wkelso02@gmail.com>
2024-07-16 13:14:03 -0400

    add jumps and associated tests

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,116 +1,236 @@
 use crate::*;
 
-pub struct InstrImm {
-    pub dest_reg:u8,
-    pub src_reg:u8,
-    pub imm:u16,
+pub trait Instruction {
+    fn execute(self, processor: &mut LC3);
 }
 
-pub struct InstrReg {
-    pub dest_reg:u8,
-    pub src_reg_1:u8,
-    pub src_reg_2:u8,
+pub struct InstrRegImm {
+    pub dest_reg: u8,
+    pub src_reg: u8,
+    pub imm: u16,
+}
+
+pub struct InstrRegReg {
+    pub dest_reg: u8,
+    pub src_reg_1: u8,
+    pub src_reg_2: u8,
+}
+
+pub struct InstrOffset6 {
+    pub target_reg: u8,
+    pub base_reg: u8,
+    pub offset: u16,
+}
+
+pub struct InstrPCOffset9 {
+    pub target_reg: u8,
+    pub pc_offset: u16,
+}
+
+pub struct InstrPCOffset11 {
+    pub pc_offset: u16,
 }
 
 pub enum IAdd {
-    Reg(InstrReg),
-    Imm(InstrImm),
+    Reg(InstrRegReg),
+    Imm(InstrRegImm),
 }
 
 pub enum IAnd {
-    Reg(InstrReg),
-    Imm(InstrImm),
+    Reg(InstrRegReg),
+    Imm(InstrRegImm),
 }
 
-pub struct INot {
-    pub dest_reg:u8,
-    pub src_reg:u8,
+pub enum INot {
+    Instr(InstrRegImm), //not actually a RegImm, just used for implementation
 }
 
+pub struct IBranch {
+    //while br roughly follows the bit assignment of PCoffset9,
+    //this is treated as a special case for ease of implementation
+    pub cond_codes: ConditionReg,
+    pub pc_offset: u16,
+}
 
-pub trait Instruction {
-    fn execute(self, processor:&mut LC3);
+pub enum IJump {
+    Instr(InstrOffset6), //not strictly an offset6, but doesn't matter here
+}
+
+pub enum IJumpSubRoutine {
+    Offset(InstrPCOffset11), //JSR
+    Reg(InstrOffset6),       //JSRR treated as an offset6 with an offset of 0
+}
+
+pub enum ISubRoutineReturn {
+    Instr(InstrOffset6), //this could be hardcoded, but it's clearer to follow the machine code
+                         //convention
+}
+
+pub enum IInterruptReturn {
+    Instr(InstrOffset6), //treated as an offset6 with an offset of 0
 }
 
 impl Instruction for IAdd {
-    fn execute(self, processor:&mut LC3) {
+    fn execute(self, processor: &mut LC3) {
         let dest;
         let result = match self {
-            Self::Reg(InstrReg{dest_reg, src_reg_1, src_reg_2}) => {
+            Self::Reg(InstrRegReg {
+                dest_reg,
+                src_reg_1,
+                src_reg_2,
+            }) => {
                 dest = dest_reg;
                 processor.regs[src_reg_1 as usize] + processor.regs[src_reg_2 as usize]
-            },
-            Self::Imm(InstrImm{dest_reg, src_reg, imm}) => {
+            }
+            Self::Imm(InstrRegImm {
+                dest_reg,
+                src_reg,
+                imm,
+            }) => {
                 dest = dest_reg;
                 processor.regs[src_reg as usize] + imm
-            },
+            }
         };
         processor.regs[dest as usize] = result;
 
         if (result as i16) > 0 {
-            processor.conds.positive = true;
-            processor.conds.zero = false;
-            processor.conds.negative = false;
+            processor.conds = ConditionReg {
+                positive: true,
+                zero: false,
+                negative: false,
+            };
         } else if (result as i16) < 0 {
-            processor.conds.positive = false;
-            processor.conds.zero = false;
-            processor.conds.negative = true;
+            processor.conds = ConditionReg {
+                positive: false,
+                zero: false,
+                negative: true,
+            };
         } else {
-            processor.conds.positive = false;
-            processor.conds.zero = true;
-            processor.conds.negative = false;
+            processor.conds = ConditionReg {
+                positive: false,
+                zero: true,
+                negative: false,
+            };
         }
     }
 }
 
 impl Instruction for IAnd {
-    fn execute(self, processor:&mut LC3) {
+    fn execute(self, processor: &mut LC3) {
         let dest;
         let result = match self {
-            Self::Reg(InstrReg{dest_reg, src_reg_1, src_reg_2}) => {
+            Self::Reg(InstrRegReg {
+                dest_reg,
+                src_reg_1,
+                src_reg_2,
+            }) => {
                 dest = dest_reg;
                 processor.regs[src_reg_1 as usize] & processor.regs[src_reg_2 as usize]
-            },
-            Self::Imm(InstrImm{dest_reg, src_reg, imm}) => {
+            }
+            Self::Imm(InstrRegImm {
+                dest_reg,
+                src_reg,
+                imm,
+            }) => {
                 dest = dest_reg;
                 processor.regs[src_reg as usize] & imm
-            },
+            }
         };
         processor.regs[dest as usize] = result;
 
         if (result as i16) > 0 {
-            processor.conds.positive = true;
-            processor.conds.zero = false;
-            processor.conds.negative = false;
+            processor.conds = ConditionReg {
+                positive: true,
+                zero: false,
+                negative: false,
+            };
         } else if (result as i16) < 0 {
-            processor.conds.positive = false;
-            processor.conds.zero = false;
-            processor.conds.negative = true;
+            processor.conds = ConditionReg {
+                positive: false,
+                zero: false,
+                negative: true,
+            };
         } else {
-            processor.conds.positive = false;
-            processor.conds.zero = true;
-            processor.conds.negative = false;
+            processor.conds = ConditionReg {
+                positive: false,
+                zero: true,
+                negative: false,
+            };
         }
     }
 }
 
 impl Instruction for INot {
-    fn execute(self, processor:&mut LC3) {
-        let result = !processor.regs[self.src_reg as usize];
-        processor.regs[self.dest_reg as usize] = result;
-
+    fn execute(self, processor: &mut LC3) {
+        let dest;
+        let result = match self {
+            Self::Instr(InstrRegImm {
+                dest_reg, src_reg, ..
+            }) => {
+                dest = dest_reg;
+                !processor.regs[src_reg as usize]
+            }
+        };
+        processor.regs[dest as usize] = result;
         if (result as i16) > 0 {
-            processor.conds.positive = true;
-            processor.conds.zero = false;
-            processor.conds.negative = false;
+            processor.conds = ConditionReg {
+                positive: true,
+                zero: false,
+                negative: false,
+            };
         } else if (result as i16) < 0 {
-            processor.conds.positive = false;
-            processor.conds.zero = false;
-            processor.conds.negative = true;
+            processor.conds = ConditionReg {
+                positive: false,
+                zero: false,
+                negative: true,
+            };
         } else {
-            processor.conds.positive = false;
-            processor.conds.zero = true;
-            processor.conds.negative = false;
+            processor.conds = ConditionReg {
+                positive: false,
+                zero: true,
+                negative: false,
+            };
         }
+    }
+}
+
+impl Instruction for IBranch {
+    fn execute(self, processor: &mut LC3) {
+        if (self.cond_codes.positive && processor.conds.positive)
+            || (self.cond_codes.zero && processor.conds.zero)
+            || (self.cond_codes.negative && processor.conds.negative)
+        {
+            processor.pc += self.pc_offset;
+        }
+    }
+}
+
+impl Instruction for IJump {
+    fn execute(self, processor: &mut LC3) {
+        let dest;
+        match self {
+            Self::Instr(InstrOffset6 { base_reg, .. }) => {
+                dest = base_reg;
+            }
+        }
+        processor.pc = processor.regs[dest as usize];
+    }
+}
+
+impl Instruction for IJumpSubRoutine {
+    fn execute(self, processor: &mut LC3) {
+        processor.regs[7] = processor.pc;
+        let jump_addr: u16;
+        match self {
+            Self::Offset(InstrPCOffset11 { pc_offset }) => {
+                //JSR
+                jump_addr = processor.pc + pc_offset;
+            }
+            Self::Reg(InstrOffset6 { base_reg, .. }) => {
+                //JSRR
+                jump_addr = processor.regs[base_reg as usize];
+            }
+        };
+        processor.pc = jump_addr;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,16 @@ pub mod instruction;
 type LC3Word = u16;
 type LC3MemAddr = u16;
 
-const ADDR_SPACE_SIZE:usize = 2_usize.pow(16_u32);  //size of the memory address space
-const TRAP_VEC_TBL:LC3Word = 0x0000;                //first address of the trap vector table
-const IR_VEC_TBL:LC3Word = 0x0100;                  //first address of the interrupt vector table
-const OS_SUPER_STACK:LC3Word =  0x0200;             //first address of the operating and supervisor
-                                                        //stack space
-const USER_SPACE:LC3Word = 0x3000;                  //first address of the user code space
-const DEV_REG_ADDR:LC3Word = 0xFE00;                //first address of the device register address
-                                                        //space
+const ADDR_SPACE_SIZE: usize = 2_usize.pow(16_u32); //size of the memory address space
+const TRAP_VEC_TBL: LC3Word = 0x0000; //first address of the trap vector table
+const IR_VEC_TBL: LC3Word = 0x0100; //first address of the interrupt vector table
+const OS_SUPER_STACK: LC3Word = 0x0200; //first address of the operating and supervisor
+                                        //stack space
+const USER_SPACE: LC3Word = 0x3000; //first address of the user code space
+const DEV_REG_ADDR: LC3Word = 0xFE00; //first address of the device register address
+                                      //space
 
-const NUM_REGS:usize = 8_usize;                        //number of registers in the LC3 spec
+const NUM_REGS: usize = 8_usize; //number of registers in the LC3 spec
 
 pub struct ConditionReg {
     pub negative: bool,
@@ -24,115 +24,327 @@ pub struct LC3 {
     pub mem: Box<[LC3Word; ADDR_SPACE_SIZE]>,
     pub conds: ConditionReg,
     pub regs: Box<[LC3Word; NUM_REGS]>,
-    pub pc:LC3MemAddr, //program counter should be initialized on startup
+    pub pc: LC3MemAddr, //program counter should be initialized on startup
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
     use super::instruction::*;
+    use super::*;
 
+    //TODO: rewrite all of these in a more intelligent fashion
     #[test]
-    fn test_add_imm() {
-        let mut processor = LC3{
+    fn instr_add_imm() {
+        let mut processor = LC3 {
             mem: Box::new([0; ADDR_SPACE_SIZE]),
-            conds: ConditionReg{negative: false, zero: false, positive: false},
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
             regs: Box::new([6, 4, 7, 10, 24, 8, 9, 18]),
             pc: 0x0000,
         };
 
-        let test_instr = IAdd::Imm(InstrImm{dest_reg:1, src_reg:0, imm:5});
+        let test_instr = IAdd::Imm(InstrRegImm {
+            dest_reg: 1,
+            src_reg: 0,
+            imm: 5,
+        });
         test_instr.execute(&mut processor);
         assert_eq!(processor.regs[1], 11)
     }
 
     #[test]
-    fn test_add_reg() {
-        let mut processor = LC3{
+    fn instr_add_reg() {
+        let mut processor = LC3 {
             mem: Box::new([0; ADDR_SPACE_SIZE]),
-            conds: ConditionReg{negative: false, zero: false, positive: false},
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
             regs: Box::new([6, 4, 7, 10, 24, 8, 9, 18]),
             pc: 0x0000,
         };
 
-        let test_instr = IAdd::Reg(InstrReg{dest_reg: 1, src_reg_1: 0, src_reg_2: 3});
+        let test_instr = IAdd::Reg(InstrRegReg {
+            dest_reg: 1,
+            src_reg_1: 0,
+            src_reg_2: 3,
+        });
         test_instr.execute(&mut processor);
         assert_eq!(processor.regs[1], 16)
     }
 
     #[test]
-    fn test_and_imm() {
-        let mut processor = LC3{
+    fn instr_and_imm() {
+        let mut processor = LC3 {
             mem: Box::new([0; ADDR_SPACE_SIZE]),
-            conds: ConditionReg{negative: false, zero: false, positive: false},
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
             regs: Box::new([6, 4, 7, 10, 24, 8, 9, 0]),
             pc: 0x0000,
         };
 
-        let test_instr = IAnd::Imm(InstrImm{dest_reg: 1, src_reg: 0, imm: 0b0000000000000000});
+        let test_instr = IAnd::Imm(InstrRegImm {
+            dest_reg: 1,
+            src_reg: 0,
+            imm: 0b0000000000000000,
+        });
         test_instr.execute(&mut processor);
         assert_eq!(processor.regs[1], 0b0000000000000000);
     }
 
     #[test]
-    fn test_and_reg() {
-        let mut processor = LC3{
+    fn instr_and_reg() {
+        let mut processor = LC3 {
             mem: Box::new([0; ADDR_SPACE_SIZE]),
-            conds: ConditionReg{negative: false, zero: false, positive: false},
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
             regs: Box::new([6, 4, 7, 10, 24, 8, 9, 0]),
             pc: 0x0000,
         };
 
-        let test_instr = IAnd::Reg(InstrReg{dest_reg: 1, src_reg_1: 0, src_reg_2: 7 });
+        let test_instr = IAnd::Reg(InstrRegReg {
+            dest_reg: 1,
+            src_reg_1: 0,
+            src_reg_2: 7,
+        });
         test_instr.execute(&mut processor);
         assert_eq!(processor.regs[1], 0b0000000000000000);
     }
 
     #[test]
-    fn test_not() {
-        let mut processor = LC3{
+    fn instr_not() {
+        let mut processor = LC3 {
             mem: Box::new([0; ADDR_SPACE_SIZE]),
-            conds: ConditionReg{negative: false, zero: false, positive: false},
-            regs: Box::new([6, 4, 7, 10, 24, 8, 9, 0]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([
+                0b0000000011111111,
+                0b1111111100000000,
+                0b0000111100001111,
+                0b1111000011110000,
+                0b0011001100110011,
+                0b1100110011001100,
+                0b0101010101010101,
+                0b1010101010101010,
+            ]),
             pc: 0x0000,
         };
 
-        let test_instr = INot{dest_reg: 1, src_reg:7};
+        let test_instr = INot::Instr(InstrRegImm {
+            dest_reg: 1,
+            src_reg: 0,
+            imm: 0,
+        });
         test_instr.execute(&mut processor);
-        assert_eq!(processor.regs[1], 0b1111111111111111);
+        assert_eq!(processor.regs[1], 0b1111111100000000);
     }
 
     #[test]
-    fn test_not_conds() {
-        let mut processor = LC3{
+    fn instr_branch() {
+        let mut processor = LC3 {
             mem: Box::new([0; ADDR_SPACE_SIZE]),
-            conds: ConditionReg{negative: false, zero: false, positive: false},
-            regs: Box::new([0b1111111111111111, 0, 0, 0b0000111100001111, 0, 0, 0, 0b1111000011110000]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([0, 0, 0, 0, 0, 0, 0, 0]),
             pc: 0x0000,
         };
 
-        //test positive
-        let test_instr = INot{dest_reg: 1, src_reg:7};
+        //there are more clever ways to write this, I don't feel like writing them
+        processor.conds = ConditionReg {
+            positive: true,
+            zero: false,
+            negative: false,
+        }; //test BRp
+        let test_instr = IBranch {
+            cond_codes: ConditionReg {
+                positive: true,
+                zero: false,
+                negative: false,
+            },
+            pc_offset: 0x0002,
+        };
         test_instr.execute(&mut processor);
-        assert_eq!(processor.regs[1], 0b0000111100001111);
-        assert_eq!(processor.conds.positive, true);
-        assert_eq!(processor.conds.zero, false);
-        assert_eq!(processor.conds.negative, false);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
 
-        //test zero
-        let test_instr = INot{dest_reg: 1, src_reg:0};
+        processor.conds = ConditionReg {
+            positive: false,
+            zero: true,
+            negative: false,
+        }; //test BRz
+        let test_instr = IBranch {
+            cond_codes: ConditionReg {
+                positive: false,
+                zero: true,
+                negative: false,
+            },
+            pc_offset: 0x0002,
+        };
         test_instr.execute(&mut processor);
-        assert_eq!(processor.regs[1], 0b0000000000000000);
-        assert_eq!(processor.conds.positive, false);
-        assert_eq!(processor.conds.zero, true);
-        assert_eq!(processor.conds.negative, false);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
 
-        //test negative
-        let test_instr = INot{dest_reg: 1, src_reg:3};
+        processor.conds = ConditionReg {
+            positive: false,
+            zero: false,
+            negative: true,
+        }; //test BRn
+        let test_instr = IBranch {
+            cond_codes: ConditionReg {
+                positive: false,
+                zero: false,
+                negative: true,
+            },
+            pc_offset: 0x0002,
+        };
         test_instr.execute(&mut processor);
-        assert_eq!(processor.regs[1], 0b1111000011110000);
-        assert_eq!(processor.conds.positive, false);
-        assert_eq!(processor.conds.zero, false);
-        assert_eq!(processor.conds.negative, true);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: true,
+            zero: true,
+            negative: false,
+        }; //test BRpz
+        let test_instr = IBranch {
+            cond_codes: ConditionReg {
+                positive: true,
+                zero: true,
+                negative: false,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: true,
+            zero: false,
+            negative: true,
+        }; //test BRpn
+        let test_instr = IBranch {
+            cond_codes: ConditionReg {
+                positive: true,
+                zero: false,
+                negative: true,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: false,
+            zero: true,
+            negative: true,
+        }; //test BRzn
+        let test_instr = IBranch {
+            cond_codes: ConditionReg {
+                positive: false,
+                zero: true,
+                negative: true,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: true,
+            zero: true,
+            negative: true,
+        }; //test BRzn
+        let test_instr = IBranch {
+            cond_codes: ConditionReg {
+                positive: true,
+                zero: true,
+                negative: true,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+    }
+
+    #[test]
+    fn instr_jmp() {
+        let mut processor = LC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([
+                0x3000, 0x0000, 0x1000, 0x0200, 0xff00, 0xfe00, 0x3000, 0x7301,
+            ]),
+            pc: 0x0000,
+        };
+
+        let mut i = 0;
+        while i < 8 {
+            let test_instr = IJump::Instr(InstrOffset6 {
+                base_reg: i,
+                target_reg: 0, //unused
+                offset: 0,     //unused
+            });
+            test_instr.execute(&mut processor);
+            assert_eq!(processor.pc, processor.regs[i as usize]);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn instr_jsr() {
+        let mut processor = LC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+            ]),
+            pc: 0x3000,
+        };
+
+        let test_instr = IJumpSubRoutine::Offset(InstrPCOffset11 { pc_offset: 0x0006 });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x3006);
+        assert_eq!(processor.regs[7], 0x3000);
+
+        processor.pc = 0x3000;
+        processor.regs[1] = 0x000A;
+        processor.regs[7] = 0x0000;
+
+        let test_instr = IJumpSubRoutine::Reg(InstrOffset6 {
+            target_reg: 0, //unused
+            base_reg: 1,
+            offset: 0, //unused
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x000A);
+        assert_eq!(processor.regs[7], 0x3000);
     }
 }
