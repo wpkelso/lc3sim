5f59a41ca962814fc7886aee18d4ed86fc100fdb
Bennett-Petzold <46354948+Bennett-Petzold@users.noreply.github.com>
2024-12-16 19:54:12 -0500

    Make LC3 a Trait (#1)
    * Move definitions to their own file
    
    * Make LC3 machines into a trait
    
    Define an `LC3` trait with all necessary interface methods. Move the
    existing `LC3` into `CoreLC3`. This allows for some alternative backends
    to hook into `Instruction` logic, e.g. an implementation only tracking
    nonzero memory to save space or an implementation layering for rollback.
    
    Also added `thiserror` as a crate to make error definitions easier.
    
    * Apply clippy lints

---
+++ src/defs.rs
@@ -0,0 +1,89 @@
+use thiserror::Error;
+
+pub type LC3Word = u16;
+pub type SignedLC3Word = i16;
+pub type LC3MemAddr = u16;
+
+pub const ADDR_SPACE_SIZE: usize = 2_usize.pow(16_u32); //size of the memory address space
+pub const TRAP_VEC_TBL: LC3Word = 0x0000; //first address of the trap vector table
+pub const IR_VEC_TBL: LC3Word = 0x0100; //first address of the interrupt vector table
+pub const OS_SUPER_STACK: LC3Word = 0x0200; //first address of the operating and supervisor
+                                            //stack space
+pub const USER_SPACE: LC3Word = 0x3000; //first address of the user code space
+pub const DEV_REG_ADDR: LC3Word = 0xFE00; //first address of the device register address
+                                          //space
+
+pub const NUM_REGS: usize = 8_usize; //number of registers in the LC3 spec
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum RegAddr {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+}
+
+impl RegAddr {
+    /// `const` optimized u8 conversion.
+    ///
+    /// Panics when u8 is out of range.
+    pub const fn panic_from_u8(value: u8) -> Self {
+        match value {
+            0 => RegAddr::Zero,
+            1 => RegAddr::One,
+            2 => RegAddr::Two,
+            3 => RegAddr::Three,
+            4 => RegAddr::Four,
+            5 => RegAddr::Five,
+            6 => RegAddr::Six,
+            7 => RegAddr::Seven,
+            _ => panic!("Argument outside of [0, 7] (the valid LC-3 registers"),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Error)]
+#[error("{0} is not in [0, 7] (the valid LC-3 registers)")]
+pub struct InvalidRegAddr(u8);
+
+impl TryFrom<u8> for RegAddr {
+    type Error = InvalidRegAddr;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(RegAddr::Zero),
+            1 => Ok(RegAddr::One),
+            2 => Ok(RegAddr::Two),
+            3 => Ok(RegAddr::Three),
+            4 => Ok(RegAddr::Four),
+            5 => Ok(RegAddr::Five),
+            6 => Ok(RegAddr::Six),
+            7 => Ok(RegAddr::Seven),
+            x => Err(InvalidRegAddr(x)),
+        }
+    }
+}
+
+impl From<RegAddr> for u8 {
+    fn from(value: RegAddr) -> Self {
+        match value {
+            RegAddr::Zero => 0,
+            RegAddr::One => 1,
+            RegAddr::Two => 2,
+            RegAddr::Three => 3,
+            RegAddr::Four => 4,
+            RegAddr::Five => 5,
+            RegAddr::Six => 6,
+            RegAddr::Seven => 7,
+        }
+    }
+}
+
+impl From<RegAddr> for usize {
+    fn from(value: RegAddr) -> Self {
+        u8::from(value) as usize
+    }
+}
---
+++ src/executors/core.rs
@@ -0,0 +1,600 @@
+use std::iter::FusedIterator;
+
+use crate::defs::{LC3MemAddr, LC3Word, RegAddr, ADDR_SPACE_SIZE, NUM_REGS};
+
+use super::{LC3MemLoc, StepFailure, LC3};
+
+#[derive(Debug, Clone, Copy)]
+struct ConditionReg {
+    pub negative: bool,
+    pub zero: bool,
+    pub positive: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct CoreLC3 {
+    mem: Box<[LC3Word; ADDR_SPACE_SIZE]>,
+    conds: ConditionReg,
+    regs: Box<[LC3Word; NUM_REGS]>,
+    pc: LC3MemAddr,
+}
+
+impl CoreLC3 {
+    pub fn new() -> Self {
+        Self {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([0; NUM_REGS]),
+            pc: 0x0000,
+        }
+    }
+}
+
+impl Default for CoreLC3 {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl LC3 for CoreLC3 {
+    fn pc(&self) -> LC3MemAddr {
+        self.pc
+    }
+    fn set_pc(&mut self, pc: LC3MemAddr) {
+        self.pc = pc
+    }
+
+    fn reg(&self, addr: RegAddr) -> LC3Word {
+        self.regs[usize::from(addr)]
+    }
+    fn set_reg(&mut self, addr: RegAddr, value: LC3Word) {
+        self.regs[usize::from(addr)] = value
+    }
+
+    fn mem(&self, addr: LC3MemAddr) -> LC3Word {
+        self.mem[addr as usize]
+    }
+    fn set_mem(&mut self, addr: LC3MemAddr, value: LC3Word) {
+        self.mem[addr as usize] = value;
+    }
+
+    fn positive_cond(&self) -> bool {
+        self.conds.positive
+    }
+    fn zero_cond(&self) -> bool {
+        self.conds.zero
+    }
+    fn negative_cond(&self) -> bool {
+        self.conds.negative
+    }
+
+    fn flag_positive(&mut self) {
+        self.conds = ConditionReg {
+            negative: false,
+            zero: false,
+            positive: true,
+        }
+    }
+    fn flag_zero(&mut self) {
+        self.conds = ConditionReg {
+            negative: false,
+            zero: true,
+            positive: false,
+        }
+    }
+    fn flag_negative(&mut self) {
+        self.conds = ConditionReg {
+            negative: true,
+            zero: false,
+            positive: false,
+        }
+    }
+
+    type FullIter<'a> = std::iter::Cloned<std::slice::Iter<'a, LC3Word>>;
+    fn iter(&self) -> Self::FullIter<'_> {
+        self.mem.iter().cloned()
+    }
+
+    type SparseIter<'a> = CoreLC3SparseIter<'a>;
+    fn sparse_iter(&self) -> Self::SparseIter<'_> {
+        CoreLC3SparseIter::new(self.iter())
+    }
+
+    fn halt(&mut self) {
+        todo!()
+    }
+
+    fn unhalt(&mut self) {
+        todo!()
+    }
+
+    fn is_halted(&self) -> bool {
+        todo!()
+    }
+
+    fn step(&mut self) -> Result<(), StepFailure> {
+        todo!()
+    }
+
+    fn populate<I: IntoIterator<Item = LC3Word>>(&mut self, start: LC3MemAddr, words: I) {
+        let mem_iter_mut = self.mem[start.into()..].iter_mut();
+        for (word, loc) in words.into_iter().zip(mem_iter_mut) {
+            *loc = word;
+        }
+    }
+}
+
+/// Sparse iterator for [`CoreLC3`].
+///
+/// Skips all nonzero elements.
+pub struct CoreLC3SparseIter<'a> {
+    iter: std::iter::Enumerate<<CoreLC3 as LC3>::FullIter<'a>>,
+}
+
+impl<'a> CoreLC3SparseIter<'a> {
+    fn new(iter: <CoreLC3 as LC3>::FullIter<'a>) -> Self {
+        Self {
+            iter: iter.enumerate(),
+        }
+    }
+}
+
+impl Iterator for CoreLC3SparseIter<'_> {
+    type Item = LC3MemLoc;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let (loc, value) = self.iter.next()?;
+            if value != 0 {
+                return Some(LC3MemLoc {
+                    loc: loc as u16,
+                    value,
+                });
+            }
+        }
+    }
+}
+
+impl DoubleEndedIterator for CoreLC3SparseIter<'_> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        loop {
+            let (loc, value) = self.iter.next_back()?;
+            if value != 0 {
+                return Some(LC3MemLoc {
+                    loc: loc as u16,
+                    value,
+                });
+            }
+        }
+    }
+}
+
+impl FusedIterator for CoreLC3SparseIter<'_> {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{defs::RegAddr, instruction::*};
+
+    //TODO: rewrite all of these in a more intelligent fashion
+    #[test]
+    fn instr_add_imm() {
+        let mut processor = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([6, 4, 7, 10, 24, 8, 9, 18]),
+            pc: 0x0000,
+        };
+
+        let test_instr = IAdd::Imm(InstrRegImm {
+            dest_reg: const { RegAddr::panic_from_u8(1) },
+            src_reg: const { RegAddr::panic_from_u8(0) },
+            imm: 5,
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.regs[1], 11)
+    }
+
+    #[test]
+    fn instr_add_reg() {
+        let mut processor = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([6, 4, 7, 10, 24, 8, 9, 18]),
+            pc: 0x0000,
+        };
+
+        let test_instr = IAdd::Reg(InstrRegReg {
+            dest_reg: const { RegAddr::panic_from_u8(1) },
+            src_reg_1: const { RegAddr::panic_from_u8(0) },
+            src_reg_2: const { RegAddr::panic_from_u8(3) },
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.regs[1], 16)
+    }
+
+    #[test]
+    fn instr_and_imm() {
+        let mut processor = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([6, 4, 7, 10, 24, 8, 9, 0]),
+            pc: 0x0000,
+        };
+
+        let test_instr = IAnd::Imm(InstrRegImm {
+            dest_reg: const { RegAddr::panic_from_u8(1) },
+            src_reg: const { RegAddr::panic_from_u8(0) },
+            imm: 0b0000000000000000,
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.regs[1], 0b0000000000000000);
+    }
+
+    #[test]
+    fn instr_and_reg() {
+        let mut processor = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([6, 4, 7, 10, 24, 8, 9, 0]),
+            pc: 0x0000,
+        };
+
+        let test_instr = IAnd::Reg(InstrRegReg {
+            dest_reg: const { RegAddr::panic_from_u8(1) },
+            src_reg_1: const { RegAddr::panic_from_u8(0) },
+            src_reg_2: const { RegAddr::panic_from_u8(7) },
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.regs[1], 0b0000000000000000);
+    }
+
+    #[test]
+    fn instr_not() {
+        let mut processor = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([
+                0b0000000011111111,
+                0b1111111100000000,
+                0b0000111100001111,
+                0b1111000011110000,
+                0b0011001100110011,
+                0b1100110011001100,
+                0b0101010101010101,
+                0b1010101010101010,
+            ]),
+            pc: 0x0000,
+        };
+
+        let test_instr = INot::Instr(InstrRegImm {
+            dest_reg: const { RegAddr::panic_from_u8(1) },
+            src_reg: const { RegAddr::panic_from_u8(0) },
+            imm: 0,
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.regs[1], 0b1111111100000000);
+    }
+
+    #[test]
+    fn instr_branch() {
+        let mut processor = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([0, 0, 0, 0, 0, 0, 0, 0]),
+            pc: 0x0000,
+        };
+
+        //there are more clever ways to write this, I don't feel like writing them
+        processor.conds = ConditionReg {
+            positive: true,
+            zero: false,
+            negative: false,
+        }; //test BRp
+        let test_instr = IBranch {
+            cond_codes: ConditionCodes {
+                positive: true,
+                zero: false,
+                negative: false,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: false,
+            zero: true,
+            negative: false,
+        }; //test BRz
+        let test_instr = IBranch {
+            cond_codes: ConditionCodes {
+                positive: false,
+                zero: true,
+                negative: false,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: false,
+            zero: false,
+            negative: true,
+        }; //test BRn
+        let test_instr = IBranch {
+            cond_codes: ConditionCodes {
+                positive: false,
+                zero: false,
+                negative: true,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: true,
+            zero: true,
+            negative: false,
+        }; //test BRpz
+        let test_instr = IBranch {
+            cond_codes: ConditionCodes {
+                positive: true,
+                zero: true,
+                negative: false,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: true,
+            zero: false,
+            negative: true,
+        }; //test BRpn
+        let test_instr = IBranch {
+            cond_codes: ConditionCodes {
+                positive: true,
+                zero: false,
+                negative: true,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: false,
+            zero: true,
+            negative: true,
+        }; //test BRzn
+        let test_instr = IBranch {
+            cond_codes: ConditionCodes {
+                positive: false,
+                zero: true,
+                negative: true,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+
+        processor.conds = ConditionReg {
+            positive: true,
+            zero: true,
+            negative: true,
+        }; //test BRzn
+        let test_instr = IBranch {
+            cond_codes: ConditionCodes {
+                positive: true,
+                zero: true,
+                negative: true,
+            },
+            pc_offset: 0x0002,
+        };
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x0002); //branch should've been taken
+        processor.pc = 0x0000; //reset pc for next test
+    }
+
+    #[test]
+    fn instr_jmp() {
+        let mut processor = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([
+                0x3000, 0x0000, 0x1000, 0x0200, 0xff00, 0xfe00, 0x3000, 0x7301,
+            ]),
+            pc: 0x0000,
+        };
+
+        for i in 0..8 {
+            let test_instr = IJump::Instr(InstrOffset6 {
+                base_reg: RegAddr::try_from(i).unwrap(),
+                target_reg: const { RegAddr::panic_from_u8(0) }, //unused
+                offset: 0,                                       //unused
+            });
+            test_instr.execute(&mut processor);
+            assert_eq!(processor.pc, processor.regs[i as usize]);
+        }
+
+        processor.regs[7] = 0x3000;
+        let test_instr: IJump = IJump::Ret;
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x3000);
+    }
+
+    #[test]
+    fn instr_jsr() {
+        let mut processor = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+            ]),
+            pc: 0x3000,
+        };
+
+        // JSR
+        let test_instr = IJumpSubRoutine::Offset(InstrPCOffset11 { pc_offset: 0x0006 });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x3006);
+        assert_eq!(processor.regs[7], 0x3000);
+
+        processor.pc = 0x3000;
+        processor.regs[1] = 0x000A;
+        processor.regs[7] = 0x0000;
+
+        // JSRR
+        let test_instr = IJumpSubRoutine::Reg(InstrOffset6 {
+            target_reg: const { RegAddr::panic_from_u8(0) }, //unused
+            base_reg: const { RegAddr::panic_from_u8(1) },
+            offset: 0, //unused
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.pc, 0x000A);
+        assert_eq!(processor.regs[7], 0x3000);
+    }
+
+    #[test]
+    fn instr_ld() {
+        let mut processor: CoreLC3 = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+            ]),
+            pc: 0x3000,
+        };
+        let test_instr: ILoad = ILoad::Std(InstrPCOffset9 {
+            target_reg: const { RegAddr::panic_from_u8(0) },
+            pc_offset: 0x0005,
+        });
+
+        //LD
+        processor.mem[0x3006] = 0xFF14;
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.regs[0], 0xFF14);
+
+        // LDI
+        processor.mem[0x3003] = 0x3004;
+        processor.mem[0x3004] = 0xFF14;
+        let test_instr: ILoad = ILoad::Indirect(InstrPCOffset9 {
+            target_reg: const { RegAddr::panic_from_u8(1) },
+            pc_offset: 0x0002,
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.regs[1], 0xFF14);
+
+        // LDR
+        processor.mem[0x300A] = 0xFF14;
+        processor.regs[2] = 0x3009;
+        let test_instr: ILoad = ILoad::Reg(InstrOffset6 {
+            target_reg: const { RegAddr::panic_from_u8(3) },
+            base_reg: const { RegAddr::panic_from_u8(2) },
+            offset: 0x0001,
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.regs[3], 0xFF14);
+
+        // LEA
+        let test_instr: ILoad = ILoad::Addr(InstrPCOffset9 {
+            target_reg: const { RegAddr::panic_from_u8(4) },
+            pc_offset: 0x000E,
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.regs[4], 0x300F);
+    }
+
+    #[test]
+    fn instr_st() {
+        let mut processor: CoreLC3 = CoreLC3 {
+            mem: Box::new([0; ADDR_SPACE_SIZE]),
+            conds: ConditionReg {
+                negative: false,
+                zero: false,
+                positive: false,
+            },
+            regs: Box::new([
+                0xFF14, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+            ]),
+            pc: 0x3000,
+        };
+
+        // ST
+        let test_instr: IStore = IStore::Std(InstrPCOffset9 {
+            target_reg: const { RegAddr::panic_from_u8(0) },
+            pc_offset: 0x0004,
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.mem[0x3005], 0xFF14);
+
+        // STI
+        processor.mem[0x3003] = 0x300A;
+        let test_instr: IStore = IStore::Indirect(InstrPCOffset9 {
+            target_reg: const { RegAddr::panic_from_u8(0) },
+            pc_offset: 0x0002,
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.mem[0x300A], 0xFF14);
+
+        // STR
+        processor.regs[1] = 0x3003;
+        let test_instr: IStore = IStore::Reg(InstrOffset6 {
+            target_reg: const { RegAddr::panic_from_u8(0) },
+            base_reg: const { RegAddr::panic_from_u8(1) },
+            offset: 0x0003,
+        });
+        test_instr.execute(&mut processor);
+        assert_eq!(processor.mem[0x3006], 0xFF14);
+    }
+}
