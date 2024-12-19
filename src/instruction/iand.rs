7c38a86be58f6a48603709a93ce63347d1e3d28a
Bennett Petzold <dansecob.dev@gmail.com>
2024-12-19 12:23:28 -0500

    Add AND tests

--- src/instruction/iand.rs
+++ src/instruction/iand.rs
@@ -68,3 +68,85 @@ impl Instruction for IAnd {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const BITMASK_5: u16 = 1 << 5;
+    const BITMASK_4: u16 = 1 << 5;
+    const BITMASK_3: u16 = 1 << 5;
+    const BITMASK_4_3: u16 = BITMASK_4 | BITMASK_3;
+    const BASE_OPCODE: u16 = (AND_OPCODE as u16) << 12;
+
+    #[test]
+    fn reject_invalid_parses() {
+        // Only invalid state (with right opcode): bit 5 is unset but bits
+        // 4 or 3 are set.
+        let invalid_parses = (BASE_OPCODE..BASE_OPCODE * 2)
+            .filter(|word| (word & BITMASK_5) == 0)
+            .filter(|word| (word & BITMASK_4_3) != 0);
+
+        for invalid in invalid_parses {
+            assert!(IAnd::parse(invalid).is_none())
+        }
+    }
+
+    #[test]
+    fn reject_invalid_opcodes() {
+        // All other opcodes
+        let invalid_opcodes = (0..LC3Word::MAX).filter(|word| (word >> 12) != AND_OPCODE as u16);
+
+        for invalid in invalid_opcodes {
+            assert!(IAnd::parse(invalid).is_none())
+        }
+    }
+
+    #[test]
+    fn parse_immediates() {
+        // With immediate flag
+        let base = BASE_OPCODE | BITMASK_5;
+
+        for dr in 0..7 {
+            let with_dr = base | (dr << 9);
+            for sr in 0..7 {
+                let with_sr = with_dr | (sr << 6);
+                for imm in 0..0b11111 {
+                    let full = with_sr | imm;
+
+                    if let IAnd::Imm(parsed) = IAnd::parse(full).unwrap() {
+                        assert_eq!(parsed.dest_reg as u16, dr);
+                        assert_eq!(parsed.src_reg as u16, sr);
+                        assert_eq!(parsed.imm, imm);
+                    } else {
+                        panic!("Must parse as immediate!")
+                    }
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn parse_reg() {
+        // Without immediate flag
+        let base = BASE_OPCODE;
+
+        for dr in 0..7 {
+            let with_dr = base | (dr << 9);
+            for sr1 in 0..7 {
+                let with_sr = with_dr | (sr1 << 6);
+                for sr2 in 0..7 {
+                    let full = with_sr | sr2;
+
+                    if let IAnd::Reg(parsed) = IAnd::parse(full).unwrap() {
+                        assert_eq!(parsed.dest_reg as u16, dr);
+                        assert_eq!(parsed.src_reg_1 as u16, sr1);
+                        assert_eq!(parsed.src_reg_2 as u16, sr2);
+                    } else {
+                        panic!("Must parse as register!")
+                    }
+                }
+            }
+        }
+    }
+}
