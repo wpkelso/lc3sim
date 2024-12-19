f36356a055e4de04364651038b177c79273edda5
Bennett Petzold <dansecob.dev@gmail.com>
2024-12-19 16:40:51 -0500

    Add trap parse test

--- src/instruction/trap.rs
+++ src/instruction/trap.rs
@@ -14,6 +14,12 @@ pub enum Trap {
 }
 pub const TRAP_OPCODE: u8 = 0b1111;
 
+const GETC: u16 = 0x20;
+const OUT: u16 = 0x21;
+const PUTS: u16 = 0x22;
+const IN: u16 = 0x23;
+const HALT: u16 = 0x24;
+
 impl Instruction for Trap {
     fn execute<P: LC3>(self, processor: &mut P) {
         unimplemented!()
@@ -23,12 +29,6 @@ impl Instruction for Trap {
     where
         Self: Sized,
     {
-        const GETC: u16 = 0x20;
-        const OUT: u16 = 0x21;
-        const PUTS: u16 = 0x22;
-        const IN: u16 = 0x23;
-        const HALT: u16 = 0x24;
-
         if (get_opcode(word) == TRAP_OPCODE) && (get_bits(word, 11, 8) == 0) {
             match get_bits(word, 7, 0) {
                 GETC => Some(Self::Getc),
@@ -41,5 +41,49 @@ impl Instruction for Trap {
         } else {
             None
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const BASE_OPCODE: u16 = (TRAP_OPCODE as u16) << 12;
+
+    const ALL_VECS: [u16; 5] = [GETC, OUT, PUTS, IN, HALT];
+
+    const FULL_11: u16 = (1 << 12) - 1;
+    const BITMASK_11_8: u16 = (FULL_11 >> 8) << 8;
+    const BITMASK_7_0: u16 = (1 << 8) - 1;
+
+    #[test]
+    fn reject_invalid_opcodes() {
+        // All other opcodes
+        let invalid_opcodes = (0..LC3Word::MAX).filter(|word| (word >> 12) != (TRAP_OPCODE as u16));
+
+        for invalid in invalid_opcodes {
+            assert!(Trap::parse(invalid).is_none())
+        }
+    }
+
+    #[test]
+    fn reject_invalid_parses() {
+        // Invalid when bottom 5 bits aren't set
+        let invalid_parses = (BASE_OPCODE..=LC3Word::MAX).filter(|word| {
+            ((word & BITMASK_11_8) != 0) || !ALL_VECS.contains(&(word & BITMASK_7_0))
+        });
+
+        for invalid in invalid_parses {
+            assert!(Trap::parse(invalid).is_none())
+        }
+    }
+
+    #[test]
+    fn parse() {
+        assert_eq!(Trap::parse(BASE_OPCODE | GETC).unwrap(), Trap::Getc);
+        assert_eq!(Trap::parse(BASE_OPCODE | OUT).unwrap(), Trap::Out);
+        assert_eq!(Trap::parse(BASE_OPCODE | PUTS).unwrap(), Trap::PutS);
+        assert_eq!(Trap::parse(BASE_OPCODE | IN).unwrap(), Trap::In);
+        assert_eq!(Trap::parse(BASE_OPCODE | HALT).unwrap(), Trap::Halt);
     }
 }
