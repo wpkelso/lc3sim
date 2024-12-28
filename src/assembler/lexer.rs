469ab05b00e6db092f515c6aa7123e839785328b
William Kelso <wkelso02@gmail.com>
2024-12-28 18:17:31 -0500

    Add wrapper function for lexer interface

--- src/assembler/lexer.rs
+++ src/assembler/lexer.rs
@@ -1,6 +1,6 @@
 use std::mem::discriminant;
 use crate::assembler::{MaybeUnresolvedInstr, Op, PseudoOp, Token};
-use anyhow::Result;
+use anyhow::{Result, Context};
 
 /// First stage of the lexer operation, where any prefix labels are stripped out 
 pub fn prefix_label_pass(token_chain: &[Token])  -> (Option<&str>, &[Token]) {
@@ -19,8 +19,15 @@ pub fn prefix_label_pass(token_chain: &[Token])  -> (Option<&str>, &[Token]) {
 
 /// Second stage of the lexer operation, where a chain of unresolved instructions is created from
 /// the asm op. If the line consists only of a comment, then an empty Vec is returned
-pub fn construct_instruction_pass(token_chain: Vec<Token>) -> Result<Vec<MaybeUnresolvedInstr>> {
+pub fn construct_instruction_pass(token_chain: &[Token]) -> Result<Vec<MaybeUnresolvedInstr>> {
     todo!();
+}
+
+/// Wrapper function to provide a cleaner API for the lexing passes 
+pub fn lexer(token_chain: &[Token]) -> (Option<&str>, Result<Vec<MaybeUnresolvedInstr>>) {
+    let (label, chain) = prefix_label_pass(token_chain);
+    let result = construct_instruction_pass(chain).context("Failed to form a valid instruction");
+    (label, result) 
 }
 
 #[cfg(test)]
