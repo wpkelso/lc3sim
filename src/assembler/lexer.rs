bfae0deac01835641327827490d4356d65ba3fea
William Kelso <wkelso02@gmail.com>
2024-12-28 15:37:52 -0500

    Add test for stripping prefix labels

--- src/assembler/lexer.rs
+++ src/assembler/lexer.rs
@@ -3,7 +3,7 @@ use crate::assembler::{MaybeUnresolvedInstr, Op, PseudoOp, Token};
 use anyhow::Result;
 
 /// First stage of the lexer operation, where any prefix labels are stripped out 
-pub fn prefix_label_pass(token_chain: &[Token])  -> (Option<&str>, &[Token]){
+pub fn prefix_label_pass(token_chain: &[Token])  -> (Option<&str>, &[Token]) {
     // we create a shell STRING variant to get it's discriminant, as this is more flexible than if
     // we hardcode the discriminant value
     let label_discriminant = discriminant(&Token::STRING("".to_string()));
@@ -21,4 +21,18 @@ pub fn prefix_label_pass(token_chain: &[Token])  -> (Option<&str>, &[Token]){
 /// the asm op. If the line consists only of a comment, then an empty Vec is returned
 pub fn construct_instruction_pass(token_chain: Vec<Token>) -> Result<Vec<MaybeUnresolvedInstr>> {
     todo!();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn lex_label_instr() {
+        let test_vec = vec![Token::STRING("LABEL1".to_string()), Token::INSTR(Op::ILLEGAL)];
+        let (label, instr) = prefix_label_pass(&test_vec);
+
+        assert_eq!(label.unwrap(), "LABEL1");
+        assert_eq!(instr[0], Token::INSTR(Op::ILLEGAL));
+    }
 }
