06e76997eda2edaeb6934724f30c17bf15ba153f
William Kelso <wkelso02@gmail.com>
2024-12-29 12:18:27 -0500

    token validation fn shell & fn inlining

--- src/assembler/lexer.rs
+++ src/assembler/lexer.rs
@@ -1,8 +1,12 @@
 use std::mem::discriminant;
 use crate::assembler::{MaybeUnresolvedInstr, Op, PseudoOp, Token};
-use anyhow::{Result, Context};
+use anyhow::{Result, bail};
+
+// All of these functions are inlined because they work on the same exact data but are split up for
+// legibility
 
 /// First stage of the lexer operation, where any prefix labels are stripped out 
+#[inline]
 pub fn prefix_label_pass(token_chain: &[Token])  -> (Option<&str>, &[Token]) {
     // we create a shell STRING variant to get it's discriminant, as this is more flexible than if
     // we hardcode the discriminant value
@@ -17,16 +21,75 @@ pub fn prefix_label_pass(token_chain: &[Token])  -> (Option<&str>, &[Token]) {
     }
 }
 
-/// Second stage of the lexer operation, where a chain of unresolved instructions is created from
-/// the asm op. If the line consists only of a comment, then an empty Vec is returned
-pub fn construct_instruction_pass(token_chain: &[Token]) -> Result<Vec<MaybeUnresolvedInstr>> {
-    todo!();
+#[inline]
+/// Validates that a chain consists of a valid sequence of tokens for a given instruction
+/// (irrespective of any value they may have), returning a boolean value to reflect that 
+/// validation. Will return an Err value if fed an invalid instruction.
+pub fn validate_instruction(operation: Op, token_chain: &[Token]) -> Result<bool> {
+    // return a false value if a sequence cannot be validated completely
+    let mut validation: bool = false;
+
+    match operation {
+        Op::ADD => (),
+        Op::AND => (),
+        Op::BR(_, _, _) => (),
+        Op::JMP => (),
+        Op::JSR => (),
+        Op::JSRR => (),
+        Op::LD => (),
+        Op::LDI => (),
+        Op::LDR => (),
+        Op::LEA => (),
+        Op::NOT => (),
+        Op::RET => (),
+        Op::RTI => (),
+        Op::ST => (),
+        Op::STI => (),
+        Op::STR => (),
+        Op::TRAP => (),
+        Op::GETC => (),
+        Op::OUT => (),
+        Op::PUTS => (),
+        Op::IN => (),
+        Op::PUTSP => (),
+        Op::HALT => (),
+        // Covers invalid or illegal instructions that were provided
+        _ => bail!("Unknown instruction, cannot validate.")
+
+    }
+
+    Ok(validation)
 }
 
-/// Wrapper function to provide a cleaner API for the lexing passes 
+#[inline]
+pub fn validate_pseudo_instruction(operation: PseudoOp, token_chain: &[Token]) {
+
+}
+
+/// Second stage of the lexer operation, where a chain of unresolved instructions is created from
+/// the asm op. If the line consists only of a comment, then an empty Vec is returned
+#[inline]
+pub fn construct_instruction_pass(token_chain: &[Token]) -> Result<Vec<MaybeUnresolvedInstr>> {
+    let result:Vec<MaybeUnresolvedInstr> = Vec::new(); 
+
+    match token_chain[0] {
+        // Validate that you can form a valid instruction first, then transform it into an
+        // instruction in a second pass
+        Token::INSTR(_) => print!(""),
+        Token::META(_) => print!(""),
+        _ => bail!("Line is invalid, does not start with an instruction!"),
+    }
+
+    Ok(result)
+}
+
+/// Wrapper function to provide a cleaner API for the lexing passes
 pub fn lexer(token_chain: &[Token]) -> (Option<&str>, Result<Vec<MaybeUnresolvedInstr>>) {
     let (label, chain) = prefix_label_pass(token_chain);
-    let result = construct_instruction_pass(chain).context("Failed to form a valid instruction");
+    let result = construct_instruction_pass(chain);
+
+    // The result gets passed on so the assembler can attatch more context to any error messages
+    // generated (i.e. the expected address of the error)
     (label, result) 
 }
 
