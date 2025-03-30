cf1b80ede24296cd6998996428fc0cae3c6917b3
Bennett-Petzold <46354948+Bennett-Petzold@users.noreply.github.com>
2025-03-30 16:13:12 +0000

    Deploying to gh-pages from @ wpkelso/lc3sim@793f70737129952d8f25a3f50a959769d1dae9f5 🚀

---
+++ docs/trait.impl/strum/trait.IntoDiscriminant.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["lc3sim_project",[["impl <a class=\"trait\" href=\"strum/trait.IntoDiscriminant.html\" title=\"trait strum::IntoDiscriminant\">IntoDiscriminant</a> for <a class=\"enum\" href=\"lc3sim_project/assembler/enum.Token.html\" title=\"enum lc3sim_project::assembler::Token\">Token</a>"],["impl <a class=\"trait\" href=\"strum/trait.IntoDiscriminant.html\" title=\"trait strum::IntoDiscriminant\">IntoDiscriminant</a> for <a class=\"enum\" href=\"lc3sim_project/defs/enum.Op.html\" title=\"enum lc3sim_project::defs::Op\">Op</a>"],["impl <a class=\"trait\" href=\"strum/trait.IntoDiscriminant.html\" title=\"trait strum::IntoDiscriminant\">IntoDiscriminant</a> for <a class=\"enum\" href=\"lc3sim_project/defs/enum.PseudoOp.html\" title=\"enum lc3sim_project::defs::PseudoOp\">PseudoOp</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[786]}
